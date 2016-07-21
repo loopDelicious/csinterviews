6c49d8b09ad656d90ca40120de80cb72e57e137f
Joyce Lin <joycelin79@gmail.com>
2016-07-21 15:35:48 -0700

    add common problems to drill

---
+++ todrill.py
@@ -0,0 +1,42 @@
+####################################################
+#### Trees
+# height of binary tree recursively
+def height(node):
+
+# print a list of all nodes at each depth of a tree
+def print_nodes(node):
+
+# check subtree, if subtree is in bigger tree
+def check_subtree(bigger, smaller):
+
+####################################################
+#### Linked Lists
+# reverse a linked list
+def rev_ll(node):
+
+# reverse a linked list in place
+def rev_ll_in_place(node):
+
+####################################################
+#### Recursion
+# Fibonacci(n)
+def Fib(n):
+# currency
+def coins(num_coins):
+
+####################################################
+#### General
+# is_balanced for parenthesis
+def is_balanced(s):
+
+# is_one_way for edits for add, delete, replace
+def one_away(word1, word2):
+
+####################################################
+#### Sorts
+# merge sort
+def merge(lst):
+# insertion sort
+def insertion(lst):
+
+
