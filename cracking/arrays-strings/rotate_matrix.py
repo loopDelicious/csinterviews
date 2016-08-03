b4c7baa4805cd233c74bc8ab000cbef1964d679c
Joyce Lin <joycelin79@gmail.com>
2016-08-03 15:09:11 -0700

    2 matrices for zero and rotate complete, string rotation outstanding

--- cracking/arrays-strings/rotate_matrix.py
+++ cracking/arrays-strings/rotate_matrix.py
@@ -1,36 +1,42 @@
 """given an image represented by an NxN matrix, where each pixel in the image is 4 bytes
 write a method to rotate the image by 90 degrees.  Can you do this in place?
 
-matrix = [
-    [0,1,1,1],
-    [0,0,1,1],
-    [0,0,0,1],
-    [0,0,0,0]
-]
+>>> matrix = [[1, 1, 1, 2],
+...           [4, 5, 6, 2],
+...           [4, 7, 8, 2],
+...           [4, 3, 3, 3]]
 
-matrix_rotation = [
-    [0,0,0,0],
-    [0,0,0,1],
-    [0,0,1,1],
-    [0,1,1,1]
-]
+>>> rotate(matrix)
+[[4, 4, 4, 1], [3, 7, 5, 1], [3, 8, 6, 1], [3, 2, 2, 2]]
 
 """
 
-def rotate(image):
+def rotate(matrix):
     """function to rotate image in NxN matrix """
 
-# top = temp
-# top = left
-# left = bottom
-# bottom = right
-# right = temp
+    n = len(matrix)
 
-    matrix[0] = temp
-    matrix[0] = matrix[3][0], matrix[2][0], matrix[1][0], matrix[0][0]
+    rotated = []
 
+    for i in range(n):
+        rotated.append([])
+        for j in range(n):
+            rotated[i].append(matrix[n-1-j][i])
+    return rotated
 
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
         print "\n*** ALL TESTS PASSED. BONER!\n"
+
+    # top = temp
+    # top = left
+    # left = bottom
+    # bottom = right
+    # right = temp
+
+    # matrix[0] = temp
+    # matrix[i][0] = [matrix[3][0], matrix[2][0], matrix[1][0], matrix[0][0]]
+    # matrix[3] = [matrix[3][3], matrix[3][2], matrix[3][1], matrix[3][0]]
+    # matrix[i][3]
+    # matrix[0]
--- cracking/arrays-strings/zero_matrix.py
+++ cracking/arrays-strings/zero_matrix.py
@@ -1,24 +1,35 @@
 """write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0
 
-matrix = [
-    [0,1,1,1],
-    [1,0,1,1],
-    [1,1,1,1],
-    [1,1,1,1]
-]
+    >>> zero([[1, 0, 3], [4, 5, 6], [7, 8, 9]])
+    [[0, 0, 0], [4, 0, 6], [7, 0, 9]]
 
-matrix_zeroed = [
-    [0,0,0,0],
-    [0,0,1,1],
-    [0,0,1,1],
-    [0,0,1,1]
-]
+    >>> zero([[1, 0, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+    [[0, 0, 0, 0], [5, 0, 7, 8], [9, 0, 11, 12]]
 
 """
 
 def zero(matrix):
     """function to zero out row and columns in MxN matrix """
 
+    nrows = len(matrix)     #y
+    ncols = len(matrix[0])  #x
+
+    if not matrix:
+        return []
+
+    clear_rows = [False] * nrows
+    clear_cols = [False] * ncols
+
+    for y in range(nrows):
+        for x in range(ncols):
+            if matrix[y][x] == 0:
+                clear_rows[y] = True
+                clear_cols[x] = True
+    for y in range(nrows):
+        for x in range(ncols):
+            if clear_rows[y] or clear_cols[x]:
+                matrix[y][x] = 0
+    return matrix
 
 
 if __name__ == '__main__':
